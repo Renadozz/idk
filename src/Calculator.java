9c5023e27b4b8961958882f8771a1c90f81956d2
Renad <451205646@students.ju.edu.sa>
2026-02-09 14:45:56 +0300

    Update Calculator: change == to .equals and replace % with

--- src/Calculator.java
+++ src/Calculator.java
@@ -34,7 +34,7 @@ public class Calculator {
     String B = null;
 
     Calculator() {
-        frame.setVisible(true);
+        //frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
@@ -81,33 +81,67 @@ public class Calculator {
                     JButton button = (JButton) e.getSource();
                     String buttonValue = button.getText();
                     if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+                        if (buttonValue.equals("=")) {
+                            if (A != null){
+                                B = displayLabel.getText();
+                                double numA = Double.parseDouble(A);
+                                double numB = Double.parseDouble(B);
 
-                    } 
+                                if (operator.equals("+")){
+                                    displayLabel.setText(removeZeroDecimal(numA + numB));
+                                }
+                                else if (operator.equals("-")){
+                                    displayLabel.setText(removeZeroDecimal(numA - numB));
+                                }
+                                else if (operator.equals("×")){
+                                    displayLabel.setText(removeZeroDecimal(numA * numB));
+                                }
+                                else if (operator.equals("÷")){
+                                    if (numB != 0){
+                                        displayLabel.setText(removeZeroDecimal(numA / numB));
+                                    }
+                                    else {
+                                        displayLabel.setText("Error");
+                                    }
+                            }}
+                        }
+                        else if ("+-×÷".contains(buttonValue)){
+                            if (operator == null) {
+                                A = displayLabel.getText();
+                                displayLabel.setText("0");
+                                B = "0";
+                        }
+                        operator = buttonValue;
+                    }
+                }
                     else if (Arrays.asList(topSymbols).contains(buttonValue)) {
-                        if (buttonValue == "AC") {
+                        if (buttonValue.equals("AC")) {
                             clearAll();
                             displayLabel.setText("0");
 
                         }
-                        else if (buttonValue == "+/-") {
+                        else if (buttonValue.equals("+/-")) {
                             double numDisplay = Double.parseDouble(displayLabel.getText());
                             numDisplay *= -1;
                             displayLabel.setText(removeZeroDecimal(numDisplay));
 
                         }
-                        else if (buttonValue == "%") {
+                        else if (buttonValue.equals("%")) {
+                            double numDisplay = Double.parseDouble(displayLabel.getText());
+                            numDisplay /= 100;
+                            displayLabel.setText(removeZeroDecimal(numDisplay));
 
                         }
                     } 
                     else {
-                        if (buttonValue == ".") {
+                        if (buttonValue.equals(".")) {
                             if (!displayLabel.getText().contains(buttonValue)) {
                                 displayLabel.setText(displayLabel.getText() + buttonValue);
                             }
 
                         }
                         else if ("0123456789".contains(buttonValue)) {
-                            if (displayLabel.getText() == "0") {
+                            if (displayLabel.getText().equals("0")) {
                                 displayLabel.setText(buttonValue);
                             } else {
                                 displayLabel.setText(displayLabel.getText() + buttonValue);
@@ -116,6 +150,7 @@ public class Calculator {
                     }
                 }
             });
+            frame.setVisible(true);
         }
     }
     void clearAll(){
